eb8c1baf2aabafbf6e3c01bc80131620fbd9d84a
martin-creator <martinlubowa@outlook.com>
2022-12-06 19:22:28 +0300

    Added secure password to user model

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'bootstrap-sass', '3.4.1'
 
+gem 'bcrypt', '3.1.13'
+
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
+
+User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
