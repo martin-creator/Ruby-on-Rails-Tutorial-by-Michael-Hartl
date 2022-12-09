f0612d7dc93fc5beac9e50b5b8660ba7046bfc17
martin-creator <martinlubowa@outlook.com>
2022-12-09 16:17:32 +0300

    Added logic to handle unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -21,6 +20,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update.
+    else
+      render "edit"
+    end
   end
 
   private
