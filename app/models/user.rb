0bdd1b1e890ee8b68bf8752c000cd187e414c30d
martin-creator <martinlubowa@outlook.com>
2022-12-09 12:53:01 +0300

    Added logic to enable login with remembering

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,11 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-  
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
 
 # User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
